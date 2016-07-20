a89224d23c4ad2929a8f949959872183a1ff77d3
Aurelien - PhiXL <aurelienphixl@aureliens-imac.local>
2016-07-20 14:46:32 +0200

    Initial Commit

---
+++ OLParser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OLParser
+//
+//  Created by Aurelien - PhiXL on 20/07/16.
+//  Copyright © 2016 Aurelien - PhiXL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OLParser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OLParser
+//
+//  Created by Aurelien - PhiXL on 20/07/16.
+//  Copyright © 2016 Aurelien - PhiXL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
